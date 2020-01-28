55da6117a5f3ff8d09897eef714d2494221d043e
Eduardo Perez <edujpp1@gmail.com>
2020-01-28 21:03:02 +0000

    The navbar was created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Suscriptores from './componentes/suscriptores/Suscriptores';
 import MostrarSuscriptor from './componentes/suscriptores/MostrarSuscriptor';
 import EditarSuscriptor from './componentes/suscriptores/EditarSuscriptor';
 import NuevoSuscriptor from './componentes/suscriptores/NuevoSuscriptor';
 
+import Navbar from './componentes/layout/Navbar';
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/suscriptores" component={Suscriptores} />
-        <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
-        <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
-        <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
-      </Switch>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/suscriptores" component={Suscriptores} />
+          <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+          <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+          <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+        </Switch>
+      </div>
     </Router>
   );
 }
