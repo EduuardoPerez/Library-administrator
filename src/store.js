ab3dd2ff60a56d18d6b0b696901eae9b9cb27203
Eduardo Perez <edujpp1@gmail.com>
2020-02-01 15:03:57 +0000

    The Firebase / Firestore configuration file was created so they can be used later

---
+++ src/store.js
@@ -0,0 +1,51 @@
+// File with the configuration for redux, firebase & firestore
+import { createStore, combineReducers, compose } from 'redux';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Configure Firestore
+const firebaseConfig = {
+  apiKey: "AIzaSyDll7XSVFSfp2P_xNfPOToiZ0LG3VS8MxE",
+  authDomain: "bibliostore-58b7d.firebaseapp.com",
+  databaseURL: "https://bibliostore-58b7d.firebaseio.com",
+  projectId: "bibliostore-58b7d",
+  storageBucket: "bibliostore-58b7d.appspot.com",
+  messagingSenderId: "731485125029",
+  appId: "1:731485125029:web:00476fd96bd2061ea3104c",
+  measurementId: "G-E950XG53DE"
+};
+
+// run firebase
+firebase.initializeApp(firebaseConfig);
+
+// configurate react-redux
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+// Compose is an enhancer that takes different functions
+// function that takes another functions
+const createStoreWithFirebase = compose(
+ reactReduxFirebase(firebase, rrfConfig),
+ reduxFirestore(firebase)
+)(createStore);
+
+// Reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Initial state
+const initialState = {};
+
+// Create the store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+  reactReduxFirebase(firebase),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
