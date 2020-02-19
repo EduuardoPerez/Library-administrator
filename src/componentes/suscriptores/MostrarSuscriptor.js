01b5b382f5b1f6fcd0ec3cbc86aa1bdb1912141a
Eduardo Perez <edujpp1@gmail.com>
2020-02-19 21:15:06 +0000

    It was made it the query of the subscriber to the DB

--- src/componentes/suscriptores/MostrarSuscriptor.js
+++ src/componentes/suscriptores/MostrarSuscriptor.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React from 'react';// Compose es una utilidad (de programación funcional)
+// que permite aplicar múltiples potenciadores de store en serie
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import Spinner from '../layout/Spinner';
 
 const MostrarSuscriptor = () => {
   return (
@@ -6,4 +14,15 @@ const MostrarSuscriptor = () => {
   );
 };
 
-export default MostrarSuscriptor;
+export default compose(
+  firestoreConnect( props => [
+    {
+      collection: 'suscriptores',
+      storeAs: 'suscriptor',
+      doc: props.match.params.id
+    }
+  ]),
+  connect(( {firestore: { ordered }}, props ) => ({
+    suscriptor: ordered.suscriptor && ordered.suscriptor[0]
+  }))
+)(MostrarSuscriptor);
