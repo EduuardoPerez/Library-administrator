12aa1a7ec2d75955ba3073f1096c9fa8e6e2d589
Eduardo Perez <edujpp1@gmail.com>
2020-02-22 16:47:58 +0000

    The subscriber is searched in Firebase through its code

--- src/componentes/libros/PrestamoLibro.js
+++ src/componentes/libros/PrestamoLibro.js
@@ -7,7 +7,53 @@ import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 
 class PrestamoLibro extends Component {
-  state={}
+  state={
+    busqueda: '',
+    resultado: {},
+    noResultados: false
+  }
+
+  // Search subscriber for code
+  buscarAlumno = e => {
+    e.preventDefault();
+
+    // Get the value to search
+    const { busqueda } = this.state
+
+    // Extract firestore
+    const { firestore } = this.props;
+
+    // Make the query
+    const coleccion = firestore.collection('suscriptores');
+    const consulta = coleccion.where("codigo", "==", busqueda).get();
+
+    // Read the results
+    consulta.then(resultado => {
+      if (resultado.empty) {
+        // No results
+        this.setState({
+          noResultados:true,
+          resultado: {}
+        })
+      } else {
+        // There are results
+        const datos = resultado.docs[0];
+        this.setState({
+          resultado: datos.data(),
+          noResultados: false
+        })
+      }
+    })
+  }
+
+  // Save code in the state
+  leerDato = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+
   render() {
     // Extract book from props
     const { libro } = this.props;
@@ -31,6 +77,7 @@ class PrestamoLibro extends Component {
           <div className="row justify-content-center">
             <div className="col-md-8">
               <form
+                onSubmit={this.buscarAlumno}
               >
                 <legend className="color-primary text-center">
                   Buscar suscriptor por código
